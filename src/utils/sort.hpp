0f24b15bdad7a2f006e7c05593972041016ba261
Leon C. <leon.cl2025@gmail.com>
2025-10-05 18:04:06 +0800

    [draft] add log

--- src/utils/sort.hpp
+++ src/utils/sort.hpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <cstdint>
 #include <algorithm>
+#include "../log.h"
 
 namespace utility {
     // Sort suffix array
@@ -14,9 +15,14 @@ namespace utility {
         const uint32_t &length_ref /* length of reference sequence */
     )
     {
+        uint32_t count = 0;
         std::sort(seq_index, seq_index + length_ref,
             [&](uint32_t a, uint32_t b)
             {
+                count++;
+                if (count % 10000 == 0) {
+                    LOGINFO("Sorting count: " << count << "\n");
+                }
                 for (uint32_t d = 0; d < length_ref; d += step)
                 {
                     char ca = seq[(a + d) % length_ref];
